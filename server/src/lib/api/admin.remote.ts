676c9ad96f7fc695b6544d3e850641952f84319e
Juan Miguel Z. Marqueses <juanmigzmarq@gmail.com>
2025-11-13 12:00:33 +0700

    Fix update on admin listing users

--- server/src/lib/api/admin.remote.ts
+++ server/src/lib/api/admin.remote.ts
@@ -2,46 +2,64 @@ import { command, form, getRequestEvent, query } from "$app/server";
 import { auth } from "$lib/auth";
 import { createUserSchema } from "$lib/schema/admin";
 import { redirect } from "@sveltejs/kit";
+import { usersListSchema } from "$lib/schema/users";
+import type z from "zod";
 
 function requireAdmin() {
-	const { locals, request } = getRequestEvent();
-	if (!locals.user) {
-		redirect(307, "/");
-	} else {
-		if (locals.user?.role !== "admin") {
-			redirect(307, "/dashboard");
-		}
-	}
-	return {
-		user: locals.user,
-		session: locals.session,
-		headers: request.headers,
-	};
+  const { locals, request } = getRequestEvent();
+  if (!locals.user) {
+    redirect(307, "/");
+  } else {
+    if (locals.user?.role !== "admin") {
+      redirect(307, "/dashboard");
+    }
+  }
+  return {
+    user: locals.user,
+    session: locals.session,
+    headers: request.headers,
+  };
 }
 
 export const listUsers = query(async () => {
-	const { headers } = requireAdmin();
-	const users = await auth.api.listUsers({
-		query: {
-			limit: 100,
-			sortBy: "_id",
-		},
-		headers: headers,
-	});
+  const { headers } = requireAdmin();
+  const users = await auth.api.listUsers({
+    query: {
+      limit: 100,
+      sortBy: "_id",
+    },
+    headers: headers,
+  });
+  let parsedUsers: z.Infer<typeof usersListSchema> = [];
+  users.users.map((user) => {
+    parsedUsers.push({
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      emailVerified: user.emailVerified,
+      image: user.image,
+      role: user.role,
+      createdAt: user.createdAt,
+      updatedAt: user.updatedAt,
+      banned: user.banned,
+      banReason: user.banReason,
+      banExpires: user.banExpires,
+    });
+  });
 
-	return users.users;
+  return parsedUsers;
 });
 
 export const createUser = command(createUserSchema, async (user) => {
-	const _ = requireAdmin();
-	console.log(user);
-	await auth.api.createUser({
-		body: {
-			email: user.email,
-			password: user.password,
-			name: user.name,
-			role: user.role,
-		},
-	});
-	await listUsers().refresh();
+  const _ = requireAdmin();
+  console.log(user);
+  await auth.api.createUser({
+    body: {
+      email: user.email,
+      password: user.password,
+      name: user.name,
+      role: user.role,
+    },
+  });
+  await listUsers().refresh();
 });
--- server/src/routes/admin/+page.svelte
+++ server/src/routes/admin/+page.svelte
@@ -1,29 +1,32 @@
 <script lang="ts">
-  import data from "./data.js";
-  import * as Sidebar from "$lib/components/ui/sidebar/index.js";
-  import AppSidebar from "$lib/components/app-sidebar.svelte";
-  import SiteHeader from "$lib/components/site-header.svelte";
-  import SectionCards from "$lib/components/section-cards.svelte";
-  import ChartAreaInteractive from "$lib/components/chart-area-interactive.svelte";
-  import DataTable from "$lib/components/data-table.svelte";
+    import data from "./data.js";
+    import * as Sidebar from "$lib/components/ui/sidebar/index.js";
+    import AppSidebar from "$lib/components/app-sidebar.svelte";
+    import SiteHeader from "$lib/components/site-header.svelte";
+    import SectionCards from "$lib/components/section-cards.svelte";
+    import ChartAreaInteractive from "$lib/components/chart-area-interactive.svelte";
+    import DataTable from "$lib/components/data-table.svelte";
+    import { listUsers } from "$lib/api/admin.remote.js";
 </script>
 
 <Sidebar.Provider
-  style="--sidebar-width: calc(var(--spacing) * 72); --header-height: calc(var(--spacing) * 12);"
+    style="--sidebar-width: calc(var(--spacing) * 72); --header-height: calc(var(--spacing) * 12);"
 >
-  <AppSidebar variant="inset" />
-  <Sidebar.Inset>
-    <SiteHeader />
-    <div class="flex flex-1 flex-col">
-      <div class="@container/main flex flex-1 flex-col gap-2">
-        <div class="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-          <!-- <SectionCards /> -->
-          <!-- <div class="px-4 lg:px-6">
-                        <ChartAreaInteractive />
-                    </div> -->
-          <DataTable {data} />
+    <AppSidebar variant="inset" />
+    <Sidebar.Inset>
+        <SiteHeader />
+        <div class="flex flex-1 flex-col">
+            <div class="@container/main flex flex-1 flex-col gap-2">
+                <div class="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+                    {#await listUsers()}
+                        <p>Something</p>
+                    {:then data}
+                        <DataTable {data} />
+                    {:catch err}
+                        <p>There are problems in fetching Data</p>
+                    {/await}
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  </Sidebar.Inset>
+    </Sidebar.Inset>
 </Sidebar.Provider>
