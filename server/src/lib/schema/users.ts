56e9470f00f718de31fd93be7f67fae1142c852d
Juan Miguel Z. Marqueses <juanmigzmarq@gmail.com>
2025-11-13 11:32:12 +0700

    Broken update

--- server/src/lib/schema/users.ts
+++ server/src/lib/schema/users.ts
@@ -2,13 +2,17 @@ import * as z from "zod";
 import { ObjectId } from "mongodb";
 
 export const usersSchema = z.object({
-  _id: z.instanceof(ObjectId),
-  name: z.string().check(z.minLength(8)),
+  id: z.string(),
+  name: z.string(),
   email: z.email(),
-  createdAt: z.string(),
-  updatedAt: z.string(),
-  role: z.enum(["gateway", "manager", "admin"]),
-  banned: z.boolean(),
+  emailVerified: z.boolean(),
+  image: z.union([z.string(), z.null(), z.undefined()]),
+  role: z.union([z.string(), z.undefined()]),
+  createdAt: z.date(),
+  updatedAt: z.date(),
+  banned: z.union([z.boolean(), z.null(), z.undefined()]),
+  banReason: z.union([z.string(), z.null(), z.undefined()]),
+  banExpires: z.union([z.date(), z.null(), z.undefined()]),
 });
 
 export const usersListSchema = usersSchema.array();
